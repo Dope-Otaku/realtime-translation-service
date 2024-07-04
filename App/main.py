70454f428c9fbd221beced4555a83cff906fa990
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-05 02:51:10 +0530

    faild

--- App/main.py
+++ App/main.py
@@ -3,12 +3,34 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 
+
+#schemas
+# from schemas import TranslationRequest
+import schemas
+
+
+
+
+
 app = FastAPI()
 
 # setup for jinja2 templates 
 templates = Jinja2Templates(directory="templates")
 
+#enable cors 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 @app.get("/index", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/translate", response_model=schemas.TranslationRequest):
+def result(self):
+    pass
