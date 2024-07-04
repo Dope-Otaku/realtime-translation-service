0e238ef3564387aba947940324a49de55aa99d7f
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-05 01:06:25 +0530

    adding our html dir into main

--- App/main.py
+++ App/main.py
@@ -1,1 +1,14 @@
-#helllo
+from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# setup for jinja2 templates 
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/index", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
